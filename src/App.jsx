19119053018bd2c1445a919af0faf5cceda325a6
patrikaxelsson84 <patrik.axelsson@utb.ecutbildning.se>
2026-06-04 08:30:57 +0200

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import Header from "./components/Header";
+import { BrowserRouter as Router } from "react-router-dom";
+
+function App() {
+    return (
+        <>
+            <Router>
+                <div className="w-full min-h-screen bg-gray-950">
+                    <Header />
+                </div>
+            </Router>
+        </>
+    );
+}
+
+export default App;

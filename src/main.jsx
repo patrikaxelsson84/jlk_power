559c0e4f50d74de39184ad7e22f70878db10da66
patrikaxelsson84 <patrik.axelsson@utb.ecutbildning.se>
2026-06-04 08:33:10 +0200

    removed css and cleaned code

--- src/main.jsx
+++ src/main.jsx
@@ -2,10 +2,6 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-import {render} from "react-dom"
-import React from 'react'
-
-
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
